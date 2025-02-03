8ae39b07918aea82cd4a7b16397c4e81a5436ba6
Fabricio <fabriciogomesdev@gmail.com>
2025-02-03 15:52:11 -0300

    subindo da lider

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+
+export function App() {
+  
+
+  return (
+  <div className='flex justify-center items-center h-screen'>
+    <Button>
+      Enviar
+    </Button>
+  </div>
+  )
+}
+
+
