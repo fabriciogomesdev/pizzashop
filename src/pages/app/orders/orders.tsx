81544193cbfa30feb83415517cee634875658e7a
Fabricio <fabriciogomesdev@gmail.com>
2025-03-24 15:30:05 -0300

    dialog e tabela pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,23 +1,19 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "@/components/ui/pagination";
 
 export function Orders() {
-    return (
-      <>
-        <Helmet title="Pedidos" />
-        <div className="flex flex-col gap-4">
-          <h1 className="flex items-center gap-3 text-3xl font-bold tracking-tight">Pedidos</h1>
-        </div>
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="flex items-center gap-3 text-3xl font-bold tracking-tight">Pedidos</h1>
+
         <div className="space-y-2.5">
-          <form className="flex items-center gap-2">
-            <span className="text-sm font-semibold">Filtros:</span>
-            <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-          </form>
-  
+          <OrderTableFilters />
+
           <div className="rounded-md border">
             {/* Adicionando table-fixed para garantir que a largura das colunas seja consistente */}
             <Table className="table-fixed">
@@ -34,16 +30,17 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length:10 }).map((_, i) => {
-                    return(
-                        <OrderTableRow key={i}/>
-                    )
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return (
+                    <OrderTableRow key={i} />
+                  )
                 })}
               </TableBody>
             </Table>
           </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10}/>
         </div>
-      </>
-    );
-  }
-  
+      </div>
+    </>
+  );
+}
